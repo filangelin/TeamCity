dd369a7c364c97621c0cfd14212d3c125b9b0e46
Filimonova Angelina <filimonovaav3@transport.mos.ru>
2024-03-09 18:07:18 +0400

    change bogy getting way

--- api/build_api.py
+++ api/build_api.py
@@ -11,13 +11,8 @@ class BuildAPI(CustomRequester):
     def check_build(self, build_id, expected_status=HTTPStatus.OK):
         return self.send_request("GET", f"/app/rest/buildTypes/id:{build_id}", expected_status_code=expected_status)
 
-    def run_build(self, build_id, expected_status=HTTPStatus.OK):
-        build_info = {
-            "buildType": {
-                "id": build_id
-            }
-        }
-        return self.send_request("POST", "/app/rest/buildQueue", data=build_info, expected_status_code=expected_status)
+    def run_build(self, build_data, expected_status=HTTPStatus.OK):
+        return self.send_request("POST", "/app/rest/buildQueue", data=build_data, expected_status_code=expected_status)
 
     def check_runned_build(self, build_id, expected_status=HTTPStatus.OK):
         return self.send_request("GET", f"/app/rest/buildQueue?locator=buildType(id:{build_id})",
