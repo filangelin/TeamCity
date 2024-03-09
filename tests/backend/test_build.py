7acd76157f83ae6b81fb1505e491d83139adb0e9
Filimonova Angelina <filimonovaav3@transport.mos.ru>
2024-03-09 21:25:28 +0400

    ads tests

--- tests/backend/test_build.py
+++ tests/backend/test_build.py
@@ -12,7 +12,7 @@ class TestBuild:
     @allure.severity(allure.severity_level.CRITICAL)
     @allure.title('Проверка создания билда')
     @allure.description('Тест проверяет создание нового билда')
-    def test_check_build_status(self, prepared_project):
+    def test_create_build(self, prepared_project):
         project_data, user = prepared_project
         with allure.step('Создание билда'):
             build_data = BuildData.create_build_data(project_data.id)
@@ -33,6 +33,38 @@ class TestBuild:
                     f"Expected build's project id - {project_data.id}, but given - {build_response.project.id}"
 
     @allure.severity(allure.severity_level.CRITICAL)
+    @allure.link('https://example.com/docs/create_project', name='Документация')
+    @allure.issue('https://issue.tracker/project/120', name='Баг-трекер')
+    @allure.testcase('https://testcase.manager/testcase/450', name='Тест-кейс')
+    @allure.title('Проверка невозможность создания билда с пустым телом')
+    @allure.description('Тест проверяет невозможность создания билда c пустым телом')
+    def test_build_create_with_empty_body(self, prepared_project):
+        with allure.step('Подготовка данных'):
+            project_data, user = prepared_project
+        with allure.step('Попытка создания проекта'):
+            created_build = user.api_object.build_api.create_build({}, expected_status=HTTPStatus.BAD_REQUEST).text
+        with allure.step('Проверка ответа о невозможности создания билда с пустым телом'):
+            with pytest.assume:
+                assert f"Build type creation request should contain project node." in created_build, \
+                    f"Expected build without body can not to be created"
+
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.title('Проверка невозможности создания одинаковых билдов')
+    @allure.description('Тест проверяет невозможность создания одинаковых билдов')
+    def test_create_existing_build(self, prepared_project):
+        project_data, user = prepared_project
+        with allure.step('Создание билда'):
+            build_data = BuildData.create_build_data(project_data.id)
+            user.api_object.build_api.create_build(build_data.model_dump())
+        with allure.step('Попытка создания идентичного билда'):
+            user.api_object.build_api.create_build(build_data.model_dump(), expected_status=HTTPStatus.BAD_REQUEST)
+            build_of_project = user.api_object.build_api.locate_builds_of_project(project_data.id).text
+            located_response = LocatedBuildsModel.model_validate_json(build_of_project)
+            with pytest.assume:
+                assert located_response.count == 1, \
+                    f"Expected project has 1 build, but given - {located_response.count} builds"
+
+    @allure.severity(allure.severity_level.CRITICAL)
     @allure.title('Проверка удаления билда')
     @allure.description('Тест проверяет удаление билда')
     def test_delete_build(self, prepared_project):
@@ -45,7 +77,7 @@ class TestBuild:
             located_response = LocatedBuildsModel.model_validate_json(build_of_project)
             with pytest.assume:
                 assert located_response.count == 1, \
-            f"Expected project has build, but given - {located_response.count} builds"
+                    f"Expected project has build, but given - {located_response.count} builds"
             with pytest.assume:
                 assert located_response.buildType[0].id == build_data.id, \
                     f"Expected project's build id - {project_data.id}, but given - {located_response.buildType[0].id}"
--- tests/backend/test_project.py
+++ tests/backend/test_project.py
@@ -1,8 +1,9 @@
+from http import HTTPStatus
+
 import allure
 import pytest
 
 from data.project_data import ProjectResponseModel
-from enums.roles import Roles
 
 
 class TestProjectCreate:
@@ -22,11 +23,49 @@ class TestProjectCreate:
             project_response = ProjectResponseModel.model_validate_json(create_project_response)
             with pytest.assume:
                 assert project_response.id == project_data.id, \
-                    f"Expected project id - {project_data.id}, but given - '{project_response.id}'"
+                    f"Expected project id - {project_data.id}, but given - {project_response.id}"
         with allure.step('Проверка наличия только что созданного проекта в списке проектов'):
             get_projects_response = user.api_object.project_api.get_project_by_locator(project_data.id).text
             received_project = ProjectResponseModel.model_validate_json(get_projects_response)
             with pytest.assume:
                 assert received_project.id == project_data.id, \
-                    f"Expected project id - {project_data.id}, but given - '{received_project.id}'"
+                    f"Expected project id - {project_data.id}, but given - {received_project.id}"
 
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.link('https://example.com/docs/create_project', name='Документация')
+    @allure.issue('https://issue.tracker/project/120', name='Баг-трекер')
+    @allure.testcase('https://testcase.manager/testcase/450', name='Тест-кейс')
+    @allure.title('Проверка невозможность создания проекта с пустым телом')
+    @allure.description('Тест проверяет невозможность создания проекта c пустым телом')
+    def test_project_create_with_empty_body(self, prepared_project):
+        with allure.step('Подготовка данных'):
+            _, user = prepared_project
+        with allure.step('Попытка создания проекта'):
+            created_project = user.api_object.project_api.create_project({}, expected_status=HTTPStatus.BAD_REQUEST).text
+        with allure.step('Проверка ответа о невозможности создания проекта с пустым телом'):
+            with pytest.assume:
+                assert f"Project name cannot be empty." in created_project, \
+                    f"Expected project without body can not to be created"
+
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.link('https://example.com/docs/create_project', name='Документация')
+    @allure.issue('https://issue.tracker/project/123', name='Баг-трекер')
+    @allure.testcase('https://testcase.manager/testcase/456', name='Тест-кейс')
+    @allure.title('Проверка невозможность создания проекта c занятым именем')
+    @allure.description('Тест проверяет невозможность создания проекта c уже занятым именем')
+    def test_project_create_with_taken_name(self, prepared_project):
+        with allure.step('Подготовка данных'):
+            project_data, user = prepared_project
+            project_data2, user = prepared_project
+        with allure.step('Создание проекта'):
+            user.api_object.project_api.create_project(project_data.model_dump())
+
+        project_data_2 = {**project_data.model_dump(), 'name': project_data.name}
+        with allure.step('Попытка создания проекта 2'):
+            user.api_object.project_api.create_project(project_data_2, expected_status=HTTPStatus.BAD_REQUEST)
+        with allure.step('Проверка, что не создался проект с занятым именем'):
+            get_project = user.api_object.project_api.get_project().json()
+            project_ids = [project.get('id') for project in get_project.get('project')]
+            with pytest.assume:
+                assert project_data2.id not in project_ids, \
+                    f"Expected project with id - {project_data2.id} doesn't exist"
