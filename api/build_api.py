742feee3b12cec77595bb3c1f378b5d85afc3ad3
Filimonova Angelina <filimonovaav3@transport.mos.ru>
2024-03-09 17:57:43 +0400

    remove init from build_api

--- api/build_api.py
+++ api/build_api.py
@@ -4,9 +4,6 @@ from custom_requester.custom_requester import CustomRequester
 
 
 class BuildAPI(CustomRequester):
-    def __init__(self, session):
-        super().__init__(session)
-        self.session = session
 
     def create_build(self, build_data, expected_status=HTTPStatus.OK):
         return self.send_request("POST", "/app/rest/buildTypes", data=build_data, expected_status_code=expected_status)
