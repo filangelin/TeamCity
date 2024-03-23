d5790c888510f010ff6de0900ce1c802d1c951df
Filimonova Angelina <filimonovaav3@transport.mos.ru>
2024-03-23 13:16:35 +0400

    add ci-cd pipelines

--- tests/frontend/test_set_up.py
+++ tests/frontend/test_set_up.py
@@ -1,6 +1,9 @@
+import pytest
+
 from pages.setup_page import SetupPage
 
 
+@pytest.mark.parametrize("browser", ['chromium'], indirect=True)
 def test_set_up(browser):
     page = SetupPage(browser)
     page.set_up()
